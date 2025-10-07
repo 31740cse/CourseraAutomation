59c426da1dd2431e4d629018a067d6bf6fd3901f
Ravi Prakash Sahu <31740cse@email.com>
2025-10-07 22:54:08 +0530

    Refined Xpath implaced

--- src/main/java/pages/SearchResultPage.java
+++ src/main/java/pages/SearchResultPage.java
@@ -13,40 +13,43 @@ import com.utils.WaitHelper;
 public class SearchResultPage {
 	private WebDriver driver;
 	private WaitHelper waitHelper;
+
 	public SearchResultPage(WebDriver driver, WaitHelper waitHelper) {
 		// TODO Auto-generated constructor stub
-		this.driver=driver;
-		this.waitHelper= waitHelper;
+		this.driver = driver;
+		this.waitHelper = waitHelper;
 	}
 
-	public void applyFilters(Map<String,ArrayList<String>> filtersrequirements) {
+	public void applyFilters(Map<String, ArrayList<String>> filtersrequirements) {
 
-		for(String key:filtersrequirements.keySet()) {
-			for(String value:filtersrequirements.get(key)) {
-				String xpath= "//*[@id='main']/div[1]/div/div/div/div/div[1]/div/div/div/div/div/div/div/div/div[@data-testid='search-filter-group-"+key+"']//div[contains(@data-testid, '"+value+"')]/label";
-				if(driver.findElements(By.xpath(xpath)).isEmpty()) {
-					driver.findElement(By.xpath("//*[@id='main']/div[1]/div/div/div/div/div[1]/div/div/div/div/div/div/div/div/div[@data-testid='search-filter-group-"+key+"']//span[contains(text(), 'Show')]")).click();
+		for (String key : filtersrequirements.keySet()) {
+			for (String value : filtersrequirements.get(key)) {
+				String xpath = "//*[@data-testid='search-filter-group-" + key + "']//div[contains(@data-testid,'"
+						+ value + "')]/label";
+				if (driver.findElements(By.xpath(xpath)).isEmpty()) {
+					driver.findElement(By.xpath(
+							"//*[@data-testid='search-filter-group-" + key + "']//span[contains(text(),'Show')]"))
+							.click();
 				}
-				if(waitHelper.isXpathIsValid(xpath)) {
-					WebElement section= driver.findElement(By.xpath(xpath));
+				if (waitHelper.isXpathIsValid(xpath)) {
+					WebElement section = driver.findElement(By.xpath(xpath));
 					section.click();
 				}
 			}
 		}
 	}
 
+	public static ArrayList<String> extractParticularSectionDetails(WebDriver driver, String section,
+			WaitHelper waitHelper) {
 
+		String path = "//*[@id='main']//div[@data-testid='search-filter-group-" + section + "']/div/div";
 
-	public static ArrayList<String> extractParticularSectionDetails(WebDriver driver, String section, WaitHelper waitHelper) {
-
-		String path= "//*[@id='main']//div[@data-testid='search-filter-group-"+section+"']/div/div";
-		
 		waitHelper.isXpathIsValid(path);
 
-		List<WebElement> textList=driver.findElements(By.xpath(path));
+		List<WebElement> textList = driver.findElements(By.xpath(path));
 
-		ArrayList<String> ans= new ArrayList<String>();
-		for(WebElement elm:textList) {
+		ArrayList<String> ans = new ArrayList<String>();
+		for (WebElement elm : textList) {
 			ans.add(elm.getText().replaceAll("\\s", " ").trim());
 		}
 
@@ -54,45 +57,40 @@ public class SearchResultPage {
 
 	}
 
+	public static void extractInfoAboutFilter(WebElement elmt, String section, WebDriver driver,
+			WaitHelper waitHelper) {
 
-	public static void extractInfoAboutFilter(WebElement elmt, String section, WebDriver driver, WaitHelper waitHelper) {
-		
-		String languageName= elmt.getText();
+		String languageName = elmt.getText();
 		elmt.click();
-		
-		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		
+
+		// try {
+		// Thread.sleep(1000);
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+
 		ArrayList<String> filterResult;
-		filterResult= SearchResultPage.extractParticularSectionDetails(driver, section, waitHelper);
+		filterResult = SearchResultPage.extractParticularSectionDetails(driver, section, waitHelper);
 
 		SearchResultPage.log(filterResult, section, languageName);
-		
-		
-		
+
 		driver.navigate().back();
-//		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		//
+		// try {
+		// Thread.sleep(1000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 
 	private static void log(ArrayList<String> filterResult, String section, String languageName) {
-		System.out.println("For "+languageName+" "+section+" information is below\n");
-		for(String ele:filterResult) {
+		System.out.println("For " + languageName + " " + section + " information is below\n");
+		for (String ele : filterResult) {
 			System.out.println(ele);
 		}
 		System.out.println("\n");
-		return;		
+		return;
 	}
-
-
 
 }
